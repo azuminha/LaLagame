6a9ab6b9483b9c13a22b88ea2c374e43dc2ff5be
azuminha <azuminha1@gmail.com>
2024-12-06 22:08:01 -0300

    Help Menu (f1 key)

---
+++ Assets/Scrpits/HelpMenu.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpMenu : MonoBehaviour
+{
+    private void Start()
+    {
+        transform.GetChild(0).gameObject.SetActive(false);
+        transform.GetChild(1).gameObject.SetActive(false);
+        transform.GetChild(2).gameObject.SetActive(false);
+    }
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+            transform.GetChild(1).gameObject.SetActive(true);
+            transform.GetChild(2).gameObject.SetActive(true);
+        }
+        if (Input.GetKeyUp(KeyCode.F1))
+        {
+            transform.GetChild(0).gameObject.SetActive(false);
+            transform.GetChild(1).gameObject.SetActive(false);
+            transform.GetChild(2).gameObject.SetActive(false);
+        }
+    }
+}
